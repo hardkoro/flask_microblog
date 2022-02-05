cd3c8a4fb263f463acb378529efca0cb45a18cc1
Евгений Коробков <hardkoro@yandex.ru>
2022-02-05 16:37:45 +0300

    Added SQLite DB models for Users & Posts

--- app/routes.py
+++ app/routes.py
@@ -24,8 +24,9 @@ def index():
 def login():
     form = LogInForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data
-        ))
+        flash(
+            f'Login requested for user {form.username.data}, '
+            + f'remember_me={form.remember_me.data}'
+        )
         return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)

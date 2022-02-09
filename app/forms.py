be3b558e5fd2950e0f6feac4a21d3d0ab9d9be67
hardkoro <eug.korobkov@gmail.com>
2022-02-09 20:23:09 +0300

    Email Support

--- app/forms.py
+++ app/forms.py
@@ -60,3 +60,16 @@ class PostForm(FlaskForm):
         validators=[DataRequired(), Length(min=1, max=140)]
     )
     submit = SubmitField('Submit')
+
+
+class ResetPasswordRequestForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    submit = SubmitField('Request Password Reset')
+
+
+class ResetPasswordForm(FlaskForm):
+    password = StringField('Password', validators=[DataRequired()])
+    password2 = StringField(
+        'Repeat Password', validators=[DataRequired(), EqualTo('password')]
+    )
+    submit = SubmitField('Request Password Reset')
